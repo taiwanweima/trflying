3311e9d9abcce2c6c655e282a5eba4af2a617579
kxity <kxity@outlook.com>
2020-11-13 20:55:49 +0800

    1

---
+++ tks.php
@@ -0,0 +1,16 @@
+<div class="thankyou-page">
+    <!-- <span class="thankyou-page__thanks">Спасибо!</span>
+    <img class="thankyou-page__smile" src="/catalog/view/theme/BurnEngine/image/smile/smile.png" alt=""> -->
+<img height="1" width="1" src="https://www.facebook.com/tr?id=000&ev=Lead&noscript=1"/> 
+
+    <div class="thankyou-page__text"
+         style="text-align:center; margin-top:100px; ">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Check_mark.svg/200px-Check_mark.svg.png"
+             alt="">
+        <h1>आपके आदेश के लिए धन्यवाद!</h1>
+        <p>
+            ऑपरेटर आपसे शीघ्र ही संपर्क करेगा
+        </p>
+    </div>
+</div>
+
